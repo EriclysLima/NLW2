1f9922e9e7a898ee4b10da97b34fd45f15928c3e
Ericlys Lima <ericlyslima05@gmail.com>
2020-08-10 20:55:26 -0300

    final_nlw

--- src/server.js
+++ src/server.js
@@ -1,15 +1,36 @@
-const express = require('express')
-const server = express()
+// servidor
+const express = require('express');
+const server = express();
 
-server.use(express.static("public"))
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
-.get("/", (req, res) => {
-    return res.sendfile(__dirname + "/views/index.html")
+// configurar nunjucks
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
-.get("/study", (req, res) => {
-    return res.sendfile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendfile(__dirname + "/views/give-classes.html")
-})
-.listen(5500)
+
+// Inicio e configuração do servidor
+server
+    //receber os dados do req.body
+    .use(express.urlencoded({
+        extended: true
+    }))
+
+    //configurar arquivos estáticos (css, scripts, imagens)
+    .use(express.static("public"))
+
+    // rotas da aplicação
+    .get('/', pageLanding)
+    .get('/study', pageStudy)
+    .get('/give-classes', pageGiveClasses)
+    .post('/save-classes', saveClasses)
+
+    // start do servidor
+    .listen(5500);
